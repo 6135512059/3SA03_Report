8c5305ffe7ffa95e12ca23cef04e74a67811904d
patiparn wannago <patiparn.wannago@gmail.com>
2020-09-17 20:27:32 +0700

    commit 03

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -7,7 +7,6 @@ function App() {
     <WordCard value="hello"/>
     </div>
     );
-   
 }
 export default App;
 
--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -3,7 +3,9 @@ import CharacterCard from './CharacterCard';
 export default function WordCard(props){
  return (
  <div>
+
 { Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+
  </div>
  );
 }
