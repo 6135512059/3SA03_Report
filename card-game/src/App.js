b9e2cd6cb76af98851221b4ac9b07bb48576d161
patiparn wannago <patiparn.wannago@gmail.com>
2020-09-17 21:17:38 +0700

    commit 05

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import WordCard from "./WordCard"
-const RM = Math.floor(Math.random() * 2) + 1; 
+const RM = Math.floor(Math.random() * 3) + 1;
 function App() {
   if ( RM == 1)
     return (
         <div>
-          { "สร้างโดย 6135512059"  }
+          { <h1>สร้างโดย 6135512059</h1>  }
           
           <WordCard value="hello"/>
          
@@ -16,11 +16,20 @@ function App() {
   if ( RM == 2)
     return (
         <div>
-          { "สร้างโดย 6135512059"   }
+          { <h1>สร้างโดย 6135512059</h1>   }
           <WordCard value="Happy"/>
             
             </div>
             );
+  if ( RM == 3)
+      return (
+          <div>
+          { <h1>สร้างโดย 6135512059</h1>   }
+            <WordCard value="SENPAIGOD"/>
+                    
+             </div>
+             );
        
 }
+
 export default App;
